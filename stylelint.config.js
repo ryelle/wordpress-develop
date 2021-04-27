bb580148e7517c9af870e33e158db8df930de01f
Kelly Dwan <ryelle@users.noreply.github.com>
2021-04-27 17:52:45 -0400

    Drop in a stylelint to flag color values

---
+++ stylelint.config.js
@@ -0,0 +1,14 @@
+const reHex = "/#[0-9a-f]{3}([0-9a-f]{3})?/i";
+const reRgba = "/rgba?\(([0-9]+.?[0-9]*,? ?){3,4}\)/i";
+
+module.exports = {
+	rules: {
+		"declaration-property-value-disallowed-list": {
+			"/^color/": [reHex, reRgba],
+			"/^border/": [reHex, reRgba],
+			"/^background/": [reHex, reRgba],
+			"/^outline/": [reHex, reRgba],
+			"/^box-shadow/": [reHex, reRgba],
+		},
+	},
+};
